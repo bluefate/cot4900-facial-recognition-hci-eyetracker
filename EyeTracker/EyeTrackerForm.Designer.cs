211596e4d3422559e19cdfb4246d6e6f4e3ac5af
bluefate <john.m.hernandez@gmail.com>
2017-04-28 00:12:58 -0400

    Restored commit: COT 4900 Facial Recognition for HCI eye tracker.
    Archive of the Emgu.CV WinForms face/eye detection project from FAU coursework.

---
+++ EyeTracker/EyeTrackerForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace EyeTracker
+{
+	partial class EyeTrackerForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.ErrorMessage = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.trackBar = new System.Windows.Forms.TrackBar();
+			this.trackerLabel = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// ErrorMessage
+			// 
+			this.ErrorMessage.AutoSize = true;
+			this.ErrorMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ErrorMessage.Location = new System.Drawing.Point(0, 0);
+			this.ErrorMessage.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.ErrorMessage.Name = "ErrorMessage";
+			this.ErrorMessage.Size = new System.Drawing.Size(0, 25);
+			this.ErrorMessage.TabIndex = 0;
+			this.ErrorMessage.Visible = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1588, 1193);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// trackBar
+			// 
+			this.trackBar.Location = new System.Drawing.Point(1496, 12);
+			this.trackBar.Maximum = 100;
+			this.trackBar.Minimum = -100;
+			this.trackBar.Name = "trackBar";
+			this.trackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+			this.trackBar.Size = new System.Drawing.Size(80, 1181);
+			this.trackBar.TabIndex = 2;
+			this.trackBar.Value = 5;
+			this.trackBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// trackerLabel
+			// 
+			this.trackerLabel.AutoSize = true;
+			this.trackerLabel.Location = new System.Drawing.Point(1368, 12);
+			this.trackerLabel.Name = "trackerLabel";
+			this.trackerLabel.Size = new System.Drawing.Size(23, 25);
+			this.trackerLabel.TabIndex = 3;
+			this.trackerLabel.Text = "0";
+			// 
+			// EyeTrackerForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1588, 1193);
+			this.Controls.Add(this.trackerLabel);
+			this.Controls.Add(this.trackBar);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.ErrorMessage);
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.Name = "EyeTrackerForm";
+			this.Text = "Eye Tracker Form";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label ErrorMessage;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TrackBar trackBar;
+		private System.Windows.Forms.Label trackerLabel;
+	}
+}
+
---
+++ EyeTracker/EyeTrackerForm.cs
@@ -0,0 +1,101 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using EyeTracker.Classes;
+using FaceDetection;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EyeTracker
+{
+	public partial class EyeTrackerForm : Form
+	{
+
+		private VideoCapture capture;
+		int valueTracked ;
+
+		public EyeTrackerForm()
+		{
+			InitializeComponent();
+
+			try
+			{
+				capture = new VideoCapture();
+				Application.Idle += new EventHandler(ProcessFrame);
+
+				valueTracked = 50;
+				trackBar.Maximum = valueTracked * 5;
+				trackBar.Minimum = -trackBar.Maximum;
+				trackBar.Value = valueTracked;
+				trackerLabel.Text = valueTracked.ToString();
+
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message);
+				return;
+			}
+		}
+
+		private void ProcessFrame(object sender, EventArgs e)
+		{
+			try
+			{
+				if (capture?.QueryFrame() is null)
+				{
+					NoCameraError();
+					return;
+				}
+
+				Mat frame = capture.QueryFrame();
+
+				ItemsDetected faces = new ItemsDetected("haarcascade_frontalface_default.xml");
+				//ItemsDetected faces = new ItemsDetected("haarcascade_frontalface_alt.xml");
+				ItemsDetected eyes = new ItemsDetected("haarcascade_eye.xml");
+				//ItemsDetected eyes = new ItemsDetected("haarcascade_eye_tree_eyeglasses.xml");
+				ItemsDetected irises = new ItemsDetected();
+
+				//DetectFace.Detect(frame, faces, eyes, irises, ref valueTracked);
+				DetectFace.Detect(frame, faces, eyes, irises,  valueTracked);
+				trackBar.Value = valueTracked;
+				trackerLabel.Text = valueTracked.ToString();
+
+				int lineThikness = 1;
+
+				foreach (Rectangle face in faces.Rectangles)
+					CvInvoke.Rectangle(frame, face, new Bgr(Color.Red).MCvScalar, lineThikness);
+				foreach (Rectangle eye in eyes.Rectangles)
+					CvInvoke.Rectangle(frame, eye, new Bgr(Color.Blue).MCvScalar, lineThikness);
+				//foreach (Circle iris in irises.Circles)
+				//	CvInvoke.Circle(frame, iris.Center, iris.Radius, new Bgr(Color.Green).MCvScalar, lineThikness);
+
+				Image<Bgr, Byte> ImageFrame = frame.ToImage<Bgr, Byte>();
+				pictureBox1.Image = ImageFrame.ToBitmap();
+			}
+			catch { return; }
+
+		}
+
+		private void ViewerClosed(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void NoCameraError()
+		{
+			ErrorMessage.Visible = true;
+			ErrorMessage.Text = "No image found. Camera may not be conencted";
+
+		}
+
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			valueTracked = trackBar.Value;
+			trackerLabel.Text = valueTracked.ToString();
+		}
+
+
+	}
+
+}
